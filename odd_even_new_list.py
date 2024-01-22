9a350050a657545f6485dfe7a9a02e6e5b397214
unknown <emgcalangian@gmail.com>
2024-01-22 21:54:58 +0800

    pseudo code

---
+++ odd_even_new_list.py
@@ -0,0 +1,10 @@
+# Given two list of numbers, write a program to create a new list such that the new list should contain odd numbers 
+# from the first list and even numbers from the second list.
+
+# pseudo code
+
+# create 2 lists and a placeholder for the new lists
+# use for i to get the odd numbers from the first list
+# use for i to get the even numbers from the second list
+# append it to the new list
+# print the new list
