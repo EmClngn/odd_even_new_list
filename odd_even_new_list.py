2257ee04452e97b1f04e41e313a6c811a625bbe1
unknown <emgcalangian@gmail.com>
2024-01-22 22:03:27 +0800

    getting the odd and even and appending to the new list

--- odd_even_new_list.py
+++ odd_even_new_list.py
@@ -6,8 +6,18 @@
 # create 2 lists and a placeholder for the new lists
 list_1 = [10, 20, 25, 30, 35]
 list_2 = [40, 45, 60, 75, 90]
-new_lists = []
+new_list = []
 # use for i to get the odd numbers from the first list
+for i in list_1:
+    if int(i) % 2 != 0:
+        # append it to the new list
+        new_list.append(i)
+
 # use for i to get the even numbers from the second list
+for i in list_2:
+    if int(i) % 2 == 0:
+        # append it to the new list
+        new_list.append(i)
+        
 # append it to the new list
 # print the new list
