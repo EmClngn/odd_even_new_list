dbf1824f8f51ad30fc816a5815a52f24c07964da
unknown <emgcalangian@gmail.com>
2024-01-22 21:56:03 +0800

    added lists

--- odd_even_new_list.py
+++ odd_even_new_list.py
@@ -4,6 +4,9 @@
 # pseudo code
 
 # create 2 lists and a placeholder for the new lists
+list_1 = [10, 20, 25, 30, 35]
+list_2 = [40, 45, 60, 75, 90]
+new_lists = []
 # use for i to get the odd numbers from the first list
 # use for i to get the even numbers from the second list
 # append it to the new list
